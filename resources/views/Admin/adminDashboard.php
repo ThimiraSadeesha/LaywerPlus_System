e5c4df2de3da341d44e59316198265c3511f6313
sooriyo <tharukaravisara@gmail.com>
2023-07-02 01:08:13 +0530

    client registration.

--- resources/views/Admin/adminDashboard.php
+++ resources/views/Admin/adminDashboard.php
@@ -980,7 +980,7 @@
                                                 <div id="emailchart"> </div>
                                             </div>
 <!--                                                <div class="mb-3 mt-4"></div>-->
-<!--                                                   															<h4 class="fs-18 font-w600">Legend</h4>-->
+<!--                                                <h4 class="fs-18 font-w600">Legend</h4>-->
 <!--                                                </div>-->
 
 <!--                                            <div id="emailchart"> </div>-->
@@ -1001,7 +1001,7 @@
 														<div>
 															<h4 class="fs-18 font-w600 mb-4 text-nowrap">Total Clients</h4>
 															<div class="d-flex align-items-center">
-																<h2 class="fs-32 font-w700 mb-0">68</h2>
+																<h2 class="fs-32 font-w700 mb-0">88</h2>
 																<span class="d-block ms-4">
 																	<svg width="21" height="11" viewbox="0 0 21 11" fill="none" xmlns="http://www.w3.org/2000/svg">
 																		<path d="M1.49217 11C0.590508 11 0.149368 9.9006 0.800944 9.27736L9.80878 0.66117C10.1954 0.29136 10.8046 0.291359 11.1912 0.661169L20.1991 9.27736C20.8506 9.9006 20.4095 11 19.5078 11H1.49217Z" fill="#09BD3C"></path>
@@ -1024,8 +1024,8 @@
 															</div>
 														</div>
 														<div class="d-flex align-items-end mt-2 pb-3 justify-content-between">
-															<span>76 left from target</span>
-															<h4 class="mb-0">42</h4>
+															<span>80 left from target</span>
+															<h4 class="mb-0">20</h4>
 														</div>
 													</div>
 												</div>
@@ -1083,38 +1083,21 @@
 													</div>
 													<div class="col-xl-6 redial col-sm-6">
 														<div id="redial"></div>
-														<span class="text-center d-block fs-18 font-w600">On Progress <small class="text-success">70%</small></span>
+														<span class="text-center d-block fs-18 font-w600">On Progress <small class="text-success">90%</small></span>
 													</div>
 												</div>
 											</div>
 										</div>
 									</div>
-									<div class="col-xl-12 col-lg-12">
-
-											</div>
-
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <!--**********************************
-            Content body end
-        ***********************************-->
+    </div>
 
-
-	</div>
-    <!--**********************************
-        Main wrapper end
-    ***********************************-->
-
-    <!--**********************************
-        Scripts
-    ***********************************-->
     <!-- Required vendors -->
     <script src="../../vendor/global/global.min.js"></script>
 	<script src="../../vendor/chart.js/Chart.bundle.min.js"></script>
---
+++ resources/views/Client/client_registration.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Registration</title>
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link href="../../css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="authincation">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center mb-4">Sign up your account</h4>
+                        <form action="index.html">
+                            <div class="mb-3">
+                                <label class="form-label"><strong>Full Name</strong></label>
+                                <input type="text" class="form-control" placeholder="Full Name">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label"><strong>Username</strong></label>
+                                <input type="text" class="form-control" placeholder="Username">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label"><strong>National ID</strong></label>
+                                <input type="text" class="form-control" placeholder="National ID">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label"><strong>Contact Number</strong></label>
+                                <input type="text" class="form-control" placeholder="Contact Number">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label"><strong>Address</strong></label>
+                                <input type="text" class="form-control" placeholder="Address">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label"><strong>Email</strong></label>
+                                <input type="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label"><strong>Password</strong></label>
+                                <input type="password" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="text-center mt-4">
+                                <button type="submit" class="btn btn-primary btn-block">Sign me up</button>
+                            </div>
+                        </form>
+                        <div class="text-center mt-3">
+                            <p>Already have an account? <a class="text-primary" href="page-login.html">Sign in</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap JS and jQuery CDN -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+<!-- Custom JS -->
+<script src="../../vendor/global/global.min.js"></script>
+<script src="../../js/custom.min.js"></script>
+<script src="../../js/dlabnav-init.js"></script>
+<script src="../../js/styleSwitcher.js"></script>
+</body>
+
+</html>
