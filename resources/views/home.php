485fa3a4db085d4148ac173b306b8ffab493b8f4
sooriyo <tharukaravisara@gmail.com>
2023-07-01 20:34:41 +0530

    dashboard created

--- resources/views/home.php
+++ resources/views/home.php
@@ -5,7 +5,7 @@
     <!-- Linking Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Linking custom CSS file -->
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/Oldstyle.css">
     <!-- Linking font-awesome icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!-- Adding background image -->
