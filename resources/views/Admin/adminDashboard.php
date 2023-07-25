823dc356ae29ee88752880c44296f4e93684d0ab
Thimira Sadeesha <sadeeshathimira2000@gmail.com>
2023-07-26 00:27:26 +0530

    Admin_Dashboard updated 4.0

--- resources/views/Admin/adminDashboard.php
+++ resources/views/Admin/adminDashboard.php
@@ -210,7 +210,7 @@ $format_progress=number_format($progressofcases,2);
                                                             <span class="d-block ms-4">
 																	<svg width="21" height="11" viewbox="0 0 21 11"
                                                                          fill="none" xmlns="http://www.w3.org/2000/svg">
-																		<path d="M1.49217 11C0.590508 11 0.149368 9.9006 0.800944 9.27736L9.80878 
+																		<path d="M1.49217 11C0.590508 11 0.149368 9.9006 0.800944 9.27736L9.80878
 																		0.66117C10.1954 0.29136 10.8046 0.291359 11.1912 0.661169L20.1991 9.27736C20.
 																		8506 9.9006 20.4095 11 19.5078 11H1.49217Z" fill="#09BD3C"></path>
 																	</svg>
@@ -237,7 +237,7 @@ $format_progress=number_format($progressofcases,2);
 
                                                     </div>
                                                 </div>
-                                                    // Get the progress bar element
+                                                // Get the progress bar element
                                                 <script>
                                                     // Get the progress bar element
                                                     const progressBar = document.getElementById("progress-bar");
@@ -255,7 +255,7 @@ $format_progress=number_format($progressofcases,2);
                                                     progressValue.textContent = lawyerCountElement;
                                                 </script>
                                             </div>
-                                            </div>
+                                        </div>
                                         <div class="col-xl-6 col-sm-6">
                                             <div class="card">
                                                 <div class="card-body d-flex px-4  justify-content-between">
@@ -304,7 +304,7 @@ $format_progress=number_format($progressofcases,2);
                                                 <div class="col-xl-6 redial col-sm-6">
                                                     <div id="redial"></div>
                                                     <span class="text-center d-block fs-18 font-w600">On Progress <small
-                                                                class="text-success"><?php echo $format_progress."%"; ?></small></span>
+                                                            class="text-success"><?php echo $format_progress."%"; ?></small></span>
                                                 </div>
                                             </div>
                                         </div>
