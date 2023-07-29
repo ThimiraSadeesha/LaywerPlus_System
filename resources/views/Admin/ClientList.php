fdf28f4ff224c6771ab9c6910dad4528890ac9e1
sooriyo <tharukaravisara@gmail.com>
2023-07-30 00:48:44 +0530

    Client_List-1.1

--- resources/views/Admin/ClientList.php
+++ resources/views/Admin/ClientList.php
@@ -1,127 +1,85 @@
 <div id="main-wrapper">
-    <?php
-    global $conn;
+    <!-- Include the sidebar -->
+    <?php include 'sidebar.php';
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $dbname = 'lawyerPlus';
 
-    include 'sidebar.php';
-    require_once 'db_connection.php';
-
-    // Check if the delete form is submitted
-    if (isset($_POST['delete_lawyers'])) {
-        $selectedLawyers = $_POST['lawyers'];
-        $lawyerIds = implode(",", $selectedLawyers);
-        $stmt = $conn->prepare("DELETE FROM lawyer WHERE lawyer_id IN ($lawyerIds)");
-
-        if ($stmt->execute()) {
-            echo json_encode(['message' => 'Selected lawyers deleted successfully!']);
-        } else {
-            echo json_encode(['error' => 'Error deleting lawyers: ' . $conn->error]);
-        }
-        exit;
+    // Connect to database
+    $conn = new mysqli($host, $user, $password, $dbname);
+    if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
     }
 
-    // Check if the status update form is submitted
-    if (isset($_POST['update_status'])) {
-        $lawyerId = $_POST['lawyer_id'];
-        $newStatus = $_POST['status'];
-        $stmt = $conn->prepare("UPDATE lawyer SET status = ? WHERE lawyer_id = ?");
-        $stmt->bind_param("si", $newStatus, $lawyerId);
-
-        if ($stmt->execute()) {
-            echo json_encode(['message' => 'Status updated successfully!']);
-        } else {
-            echo json_encode(['error' => 'Error updating status: ' . $conn->error]);
+    // Helper function to get count from query
+    function getCount($conn, $query)
+    {
+        $result = $conn->query($query);
+        if ($result && $result->num_rows === 1) {
+            return $result->fetch_assoc()['completed_count'];
         }
-        exit;
+        return 0;
     }
 
-
-    // Retrieve data for the first table
-    $sql = "SELECT * FROM lawyer";
-    $result = $conn->query($sql);
     ?>
+
+    <!-- Content body start -->
     <div class="content-body">
         <div class="container-fluid">
-
-            <div class="row page-titles">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item active"><a>Administrator</a></li>
-                    <li class="breadcrumb-item"><a>Active Lawyers</a></li>
-                </ol>
-            </div>
-
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
                             <div class="table-responsive">
-                                <form method="post" action="">
-                                    <table class="table table-sm mb-0">
-                                        <thead>
-                                        <tr>
-                                            <th class="align-middle">
-                                            </th>
-                                            <th class="align-middle">Lawyer ID</th>
-                                            <th class="align-middle">Lawyer Name</th>
-                                            <th class="align-middle">Title</th>
-                                            <th class="align-middle">Email</th>
-                                            <th class="align-middle">Category</th>
-                                            <th class="align-middle">Contact</th>
-                                            <th class="align-middle">Status</th>
-                                            <th class="align-middle">Action</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php
-                                        while ($row = $result->fetch_assoc()) {
-                                            // Access data from the row
-                                            $lawyer_id = $row['lawyer_id'];
-                                            $title = $row['title'];
-                                            $name = $row['name'];
-                                            $email = $row['email'];
-                                            $category = $row['category'];
-                                            $contact_number = $row['contact_number'];
-                                            $status = $row['status'];
+                                <table class="table table-sm mb-0">
+                                    <thead>
+                                    <tr>
 
-                                            // Define the icons based on the status
-                                            $statusIcon = "";
-                                            switch ($status) {
-                                                case "Inactive":
-                                                case "Hold":
-                                                case "Block":
-                                                case "Active":
-                                                    break;
-                                                default:
-                                                    // Default case if status doesn't match any of the above
-                                                    $statusIcon = "";
-                                                    break;
+                                        <th class="align-middle">Client ID</th>
+                                        <th class="align-middle">Name</th>
+                                        <th class="align-middle">NIC</th>
+                                        <th class="align-middle">Email</th>
+                                        <th class="align-middle">DOB</th>
+                                        <th class="align-middle">Contact Number</th>
+                                        <th class="align-middle">Address</th>
+                                        <th class="align-middle">Password</th>
+                                        <th class="align-middle">Registered Date</th>
+                                        <th class="align-middle">Status</th>
+                                        <th class="align-middle">Action</th>
+
+
+
+
+                                    </tr>
+                                    </thead>
+                                    <tbody id="orders">
+                                    <?php
+                                    $sql = "SELECT * FROM `client` ";
+                                    $result = $conn->query($sql);
+                                    if ($result->num_rows > 0) {
+                                        while ($row = $result->fetch_assoc()) {
+                                            echo "<tr>";
+                                            echo "<tr>";
+
+                                            echo "<td>" . $row['client_id'] . "</td>";
+                                            echo "<td>" . $row['name'] . "</td>";
+                                            echo "<td>" . $row['nic'] . "</td>";
+                                            echo "<td>" . $row['email'] . "</td>";
+                                            echo "<td>" . $row['DOB'] . "</td>";
+                                            echo "<td>" . $row['contact_number'] . "</td>";
+                                            echo "<td>" . $row['address'] . "</td>";
+                                            echo "<td>" . $row['password'] . "</td>";
+                                            echo "<td>" . $row['registerd_datte'] . "</td>";
+                                            echo "<td>";
+                                            if ($row['status'] == 'active') {
+                                                echo "<span class='badge badge-success'>Active</span>";
+                                            } elseif ($row['status'] == 'blocked') {
+                                                echo "<span class='badge badge-danger'>Block</span>";
+                                            }  else {
+                                                echo $row['status']; // Fallback in case of unexpected status values
                                             }
-                                            // Display the row data for the first table
-                                            echo '<tr class="btn-reveal-trigger">';
-                                            echo '<td class="py-2">
-                                                        <div class="form-check custom-checkbox checkbox-success">
-                                                            <input type="checkbox" class="form-check-input delete-checkbox" data-lawyer-id="' . $lawyer_id . '">
-                                                        </div>
-                                                    </td>';
-                                            echo '<td class="py-2">
-                                                        <a href="#">
-                                                            <strong>' . $lawyer_id . '</strong>
-                                                        </a>
-                                                    </td>';
-                                            echo '<td class="py-2">' . $name . '</td>';
-                                            echo '<td class="py-2">' . $title . '</td>';
-                                            echo '<td class="py-2">' . $email . '</td>';
-                                            echo '<td class="py-2">' . $category . '</td>';
-                                            echo '<td class="py-2">' . $contact_number . '</td>';
-                                            echo '<td class="py-2">';
-                                            if ($status == "Active") {
-                                                echo '<span class="badge badge-success">' . $status . $statusIcon . '</span></td>';
-                                            } elseif ($status == "Inactive") {
-                                                echo '<span class="badge badge-primary"> ' . $status . $statusIcon . '</span></td>';
-                                            } elseif ($status == "Hold") {
-                                                echo '<span class="badge badge-warning"> ' . $status . $statusIcon . '</span></td>';
-                                            } elseif ($status == "Block") {
-                                                echo '<span class="badge badge-danger">' . $status . $statusIcon . '</span></td>';
-                                            }
+                                            echo "</td>";
                                             echo '<td class="py-2">
                                                         <div class="dropdown text-sans-serif">
                                                             <button class="btn btn-primary tp-btn-light sharp" type="button" id="order-dropdown-0" data-bs-toggle="dropdown" data-boundary="viewport" aria-haspopup="true" aria-expanded="false">
@@ -140,59 +98,28 @@
                                                                 <div class="py-2">
                                                                     <a class="dropdown-item" href="javascript:void(0);">Active</a>
                                                                     <div class="dropdown-divider"></div>
-                                                                    <a class="dropdown-item" href="javascript:void(0);">Inactive</a>
-                                                                    <div class="dropdown-divider"></div>
-                                                                    <a class="dropdown-item text-warning" href="javascript:void(0);">Hold</a>
-                                                                    <div class="dropdown-divider"></div>
-                                                                    <a class="dropdown-item text-danger" href="javascript:void(0);">Block</a>
-                                                                    <div class="dropdown-divider"></div>
+                                                                    <a class="dropdown-item" href="javascript:void(0);">Block</a>
+                                                                    <div class="dropdown-divider"></div> 
                                                                     <a class="dropdown-item text-danger delete-link" href="javascript:void(0);">Delete</a>
                                                                 </div>
                                                             </div>
                                                         </div>
                                                     </td>';
                                             echo '</tr>';
+
+
+                                            echo "</tr>";
+
                                         }
-                                        ?>
-                                        </tbody>
-                                    </table>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Deleted records Table -->
-        <div class="container-fluid">
-            <div class="row page-titles">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item active"><a>Administrator</a></li>
-                    <li class="breadcrumb-item"><a>Deleted Lawyers</a></li>
-                </ol>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <form method="post" action="">
-                                    <table class="table table-sm mb-0">
-                                        <thead>
-                                        <tr>
-                                            <th class="align-middle"></th>
-                                            <th class="align-middle">Lawyer ID</th>
-                                            <th class="align-middle">Name</th>
-                                            <th class="align-middle">Email</th>
-                                            <th class="align-middle">Contact</th>
-                                            <th class="align-middle">Status</th>
-                                            <th class="align-middle">Action</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        </tbody>
-                                    </table>
-                                </form>
+
+                                    } else {
+                                        echo "<tr><td colspan='7'>No data found</td></tr>";
+                                    }
+
+
+                                    ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
@@ -200,79 +127,5 @@
             </div>
         </div>
     </div>
+
 </div>
-
-<script>
-    function checkAllCheckboxes() {
-        // Get the value of the "Check All" checkbox
-        var checkAllCheckbox = document.getElementById("checkAll");
-        var isChecked = checkAllCheckbox.checked;
-
-        // Get all other checkboxes
-        var checkboxes = document.getElementsByClassName("delete-checkbox");
-
-        // Loop through all checkboxes and set their state based on the "Check All" checkbox
-        for (var i = 0; i < checkboxes.length; i++) {
-            // Check the first checkbox if it is the "Check All" checkbox or if it has the data-lawyer-id attribute set to "header"
-            if (checkboxes[i] === checkAllCheckbox || checkboxes[i].dataset.lawyerId === "header") {
-                checkboxes[i].checked = isChecked;
-            } else {
-                // Check other checkboxes only if the "Check All" checkbox is checked
-                if (isChecked) {
-                    checkboxes[i].checked = true;
-                } else {
-                    checkboxes[i].checked = false;
-                }
-            }
-        }
-    }
-
-    $(document).ready(function () {
-        // Attach a click event handler to the delete button
-        $(".delete-link").on("click", function () {
-            // Get the lawyer ID from the data attribute
-            var lawyerId = $(this).closest("tr").find(".delete-checkbox").data("lawyer-id");
-
-            // Send the delete request to the server using AJAX
-            $.ajax({
-                url: "",
-                type: "POST",
-                data: {lawyers: [lawyerId], delete_lawyers: true},
-                success: function (response) {
-                    console.log("Selected lawyer deleted successfully!");
-                    location.reload(); // Reload the page after successful deletion
-                },
-                error: function (error) {
-                    // Handle errors if the deletion fails
-                    console.error("Error deleting lawyer:", error);
-                }
-            });
-        });
-
-        // Attach a click event handler to the status options
-        $(".dropdown-item[href='javascript:void(0);']").on("click", function () {
-            // Get the lawyer ID from the data attribute
-            var lawyerId = $(this).closest("tr").find(".delete-checkbox").data("lawyer-id");
-
-            // Get the selected status
-            var newStatus = $(this).text();
-
-            // Send the status update request to the server using AJAX
-            $.ajax({
-                url: "",
-                type: "POST",
-                data: {lawyer_id: lawyerId, status: newStatus, update_status: true},
-                success: function (response) {
-                    // Handle the response from the server if needed
-                    console.log("Status updated successfully!");
-                    location.reload();
-                },
-                error: function (error) {
-                    console.error("Error updating status:", error);
-                }
-
-            });
-        });
-    });
-</script>
-
