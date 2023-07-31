d642e79885da3ae76468235a08635497cf4b3055
Thimira Sadeesha <sadeeshathimira2000@gmail.com>
2023-07-31 19:22:13 +0530

    Client Case List Done

--- resources/views/Client/ClientCaseList.php
+++ resources/views/Client/ClientCaseList.php
@@ -1,2 +1,84 @@
-<?php
-include "sidebar.php";
+<div id="main-wrapper">
+    <?php
+    include "sidebar.php";
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $dbname = 'lawyerPlus';
+
+    // Connect to database
+    $conn = new mysqli($host, $user, $password, $dbname);
+    if ($conn->connect_error) {
+        die('Connection failed: ' . $conn->connect_error);
+    }
+
+    $query = "SELECT c.`case_id`, c.`lawyer_id`, c.`C_type`, c.`submit_date`, c.`satuts`, c.`Amount`, l.`title`, l.`name`, l.`category`
+          FROM `case` c
+          LEFT JOIN `lawyer` l ON c.`lawyer_id` = l.`lawyer_id`
+          WHERE c.`client_id` = 'E2046014'";
+
+    ?>
+
+    <div class="content-body">
+        <div class="container-fluid">
+            <div class="row page-titles">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active"><a>Client</a></li>
+                    <li class="breadcrumb-item"><a>All Cases</a></li>
+                </ol>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-sm mb-0">
+                                    <thead>
+                                    <tr>
+                                        <th class="align-middle"><strong>Case ID</strong></th>
+                                        <th class="align-middle"><strong>Lawyer Name</strong></th>
+                                        <th class="align-middle"><strong>Case Type</strong></th>
+                                        <th class="align-middle"><strong>Submit Date</strong></th>
+                                        <th class="align-middle"><strong>Status</strong></th>
+                                        <th class="align-middle"><strong>Amount</strong></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody id="orders">
+                                    <?php
+                                    $client_CaseList = $conn->query($query);
+                                    if ($client_CaseList && $client_CaseList->num_rows > 0) {
+                                        while ($row = $client_CaseList->fetch_assoc()) {
+                                            $Case_ID = $row['case_id'];
+                                            $law_id = $row['lawyer_id'];
+                                            $date = $row['submit_date'];
+                                            $status = $row['satuts'];
+                                            $l_type = $row['category'];
+                                            $c_type = $row['C_type'];
+                                            $amount = $row['Amount'];
+                                            $title = $row['title'];
+                                            $name = $row['name'];
+                                            ?>
+                                            <tr>
+                                                <td class="align-middle"><?php echo $Case_ID; ?></td>
+                                                <td class="align-middle"><?php echo $name; ?></td>
+                                                <td class="align-middle"><?php echo $c_type; ?></td>
+                                                <td class="align-middle"><?php echo $date; ?></td>
+                                                <td class="align-middle"><?php echo $status; ?></td>
+                                                <td class="align-middle"><?php echo $amount; ?></td>
+                                            </tr>
+
+                                            <?php
+                                        }
+                                    } else {
+                                        echo "No case or lawyer found for the specified client_id.";
+                                    }
+                                    ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
