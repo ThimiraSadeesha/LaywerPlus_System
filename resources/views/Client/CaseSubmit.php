94a946702220c5dd920719a430eb31b883605a42
Thimira Sadeesha <sadeeshathimira2000@gmail.com>
2023-08-08 00:00:10 +0530

    Case Submit Done

--- resources/views/Client/CaseSubmit.php
+++ resources/views/Client/CaseSubmit.php
@@ -25,9 +25,54 @@
     } else {
         $caseOptions = "<option value='' disabled selected>No case categories found</option>";
     }
+    $last_numeric_id = null;
+    do {
+        $middleNumber = str_pad($last_numeric_id + 1, 4, '0', STR_PAD_LEFT);
+        $caseID_id = "CSE-" . $middleNumber;
 
+        // Check if the client_id is already taken by an active client or a deleted client
+        $check_query = "(SELECT case_id FROM cases WHERE case_id = '$caseID_id')
+                UNION
+                (SELECT case_id FROM cases WHERE client_id = '$caseID_id')";
+
+        $check_result = $conn->query($check_query);
+
+        if ($check_result->num_rows === 0) {
+            break;
+        }
+        $last_numeric_id++;
+
+    }while(true);
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        if (isset($_POST["category"])) {
+            $selectedCategory = $_POST["category"];
+            $selectedLawyer = $_POST["lawyer"];
+            $description = $_POST["description"];
+            $date = $_POST["dates"];
+            $formattedDate = date("Y-m-d", strtotime($date));
+            $phoneNumber = $_POST["phoneNumber"];
+            $email = $_POST["email"];
+
+        }
+        $qury="SELECT `lawyer_id` FROM `lawyer` WHERE `category`='$selectedCategory' AND `name`='$selectedLawyer'";
+        $result = $conn->query($qury);
+        $row = $result->fetch_assoc();
+        $Lawyerid=$row['lawyer_id'];
+
+
+        $sql="INSERT INTO `cases`(`case_id`, `lawyer_id`, `client_id`, `description`, `C_type`, `submit_date`, `satuts`,`Amount`) VALUES ('$caseID_id','$Lawyerid','CLT-0006','$description','$selectedCategory','$formattedDate','Pending','8970')";
+        $result = $conn->query($sql);
+        if ($result) {
+            echo "<script>alert('Case Submitted Successfully')</script>";
+        } else {
+            echo "<script>alert('Case Submission Failed')</script>";
+        }
+
+    }
 
     $conn->close();
+
     ?>
     <link href="../../vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
     <div class="content-body">
@@ -42,12 +87,13 @@
                 <div class="col-xl-12 col-xxl-12">
                     <div class="card">
                         <div class="card-body">
-                            <form>
+                            <form method="POST">
                                 <div class="row">
                                     <div class="col-lg-6 mb-2">
                                         <div class="mb-3">
                                             <label class="text-label form-label">Case Category</label>
-                                            <select name="category" class="form-control" id="categorySelect" required="" onchange="updateLawyerOptions(this.value)">
+                                            <select name="category" class="form-control" id="categorySelect" required=""
+                                                    onchange="updateLawyerOptions(this.value)">
                                                 <?php echo $caseOptions; ?>
                                             </select>
                                         </div>
@@ -70,7 +116,7 @@
                                     <div class="col-lg-6 mb-2">
                                         <div class="mb-3">
                                             <label class="text-label form-label">Next Court date</label>
-                                            <input type="date" class="form-control" id="date" required="">
+                                            <input type="date" class="form-control" id="dates" required="">
                                         </div>
                                     </div>
                                     <div class="col-lg-6 mb-2">
@@ -90,11 +136,15 @@
                                 <div class="row mt-4">
                                     <div class="col-12" align="center">
                                         <label>
-                                            <div class="alert alert-danger text-center" role="alert"> By clicking the "Submit Case" button, I confirm that I have reviewed the
-                                                provided details and affirm their accuracy for the purpose of submitting this case.</div>
+                                            <div class="alert alert-danger text-center" role="alert"> By clicking the
+                                                "Submit Case" button, I confirm that I have reviewed the
+                                                provided details and affirm their accuracy for the purpose of submitting
+                                                this case.
+                                            </div>
                                         </label>
                                         <div class="text-center">
-                                            <button type="submit" class="btn btn-primary" id="submitBtn">Submit Case</button>
+                                            <button type="submit" class="btn btn-primary" id="submitBtn">Submit Case
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -113,19 +163,16 @@
     function updateLawyerOptions(category) {
         $.ajax({
             url: 'GetLawyers.php',
-            type: 'GET', 
-            data: { category: category }, // Data to be sent along with the request
-            success: function(response) {
-                $('#lawyerSelect').html(response);  // Update the HTML element with id 
+            type: 'GET',
+            data: {category: category}, // Data to be sent along with the request
+            success: function (response) {
+                $('#lawyerSelect').html(response);  // Update the HTML element with id
             }
         });
     }
 </script>
 
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Include JS -->
 <script src="../../vendor/global/global.min.js"></script>
 <script src="../../vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
 <script src="../../vendor/select2/js/select2.full.min.js"></script>
