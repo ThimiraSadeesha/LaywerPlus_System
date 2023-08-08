9f7c0e34665e27b4a1200b961b3a05d5b5c2fc81
Thimira Sadeesha <sadeeshathimira2000@gmail.com>
2023-08-09 00:35:57 +0530

    Client Appointment Encrypt Done

--- resources/views/Client/ClientAppointment.php
+++ resources/views/Client/ClientAppointment.php
@@ -46,6 +46,9 @@
         $lawyerId = $_POST['lawyer'];
         $dateTime = $_POST['time'];
         $description = $_POST['description'];
+        $secretKey = "Lawyer_Plus_System";
+
+
 
         // Generate appointment ID
         $last_numeric_id = null;
@@ -62,10 +65,13 @@
             $last_numeric_id++;
 
         } while (true);
+        $iv = random_bytes(16);
+        $encrypted = openssl_encrypt($description, "AES-256-CBC", $secretKey, 0, $iv);
+        $encriptDescription= base64_encode($iv . $encrypted);
 
         // Insert appointment data into the appointment table
         $insertQuery = "INSERT INTO `appointment` (`Appointment_id`, `Case_Type`, `Lawyer_Id`, `Description`, `client_id`, `time`)
-                    VALUES ('$appointmentId', '$caseType', '$lawyerId', '$description', NULL, '$dateTime')";
+                    VALUES ('$appointmentId', '$caseType', '$lawyerId', '$encriptDescription', NULL, '$dateTime')";
 
         if ($conn->query($insertQuery) === TRUE) {
             echo "Appointment created successfully.";
@@ -91,12 +97,12 @@
                             <div class="row">
                                 <div class="col-auto">
                                     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                                        <div class="mb-3">
-                                            <select name="category" class="form-control" id="caseType" required="" onchange="updateLawyerOptions(this.value)">
-                                                <option value="" disabled selected>Select a category</option>
-                                                <?php echo $CatogeryOptions; ?>
-                                            </select>
-                                        </div>
+                                    <div class="mb-3">
+                                        <select name="category" class="form-control" id="caseType" required="" onchange="updateLawyerOptions(this.value)">
+                                            <option value="" disabled selected>Select a category</option>
+                                            <?php echo $CatogeryOptions; ?>
+                                        </select>
+                                    </div>
                                 </div>
                                 <div class="col-auto">
                                     <div class="mb-3">
@@ -105,23 +111,23 @@
                                         </select>
                                     </div>
                                 </div>
-                                <div class="col-auto">
-                                    <div class="mb-3">
-                                        <select name="time" class="form-control" id="dateAndTimeDropdown" required="">
-                                            onchange="updateLawyerOptions(this.value)">
-                                            <option value="" disabled selected></i>Select Date & Time</option>
-                                        </select>
-                                    </div>
+                                    <div class="col-auto">
+                                        <div class="mb-3">
+                                            <select name="time" class="form-control" id="dateAndTimeDropdown" required="">
+                                                    onchange="updateLawyerOptions(this.value)">
+                                                <option value="" disabled selected></i>Select Date & Time</option>
+                                            </select>
+                                        </div>
                                 </div>
                                 <div class="basic-form">
                                     <textarea name="description" id="textarea" class="form-control input-default fixed-textarea"
                                               placeholder="Type your notice here."></textarea>
-                                    <br>
-                                    <button type="submit" name="submit" class="btn btn-primary btn-sm">Send
-                                        Statement
-                                    </button>
+                                        <br>
+                                        <button type="submit" name="submit" class="btn btn-primary btn-sm">Send
+                                            Statement
+                                        </button>
                                 </div>
-
+                              
                                 <style>
                                     .fixed-textarea {
                                         resize: none;
@@ -191,4 +197,5 @@
         }
     }
 </script>
-//re
+//
+//
