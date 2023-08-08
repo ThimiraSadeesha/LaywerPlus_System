63d258352b800412c374a2147ed819fc65e3ff22
sooriyo <tharukaravisara@gmail.com>
2023-08-08 23:33:07 +0530

    ClientAppointment.php reupload

--- resources/views/Client/ClientAppointment.php
+++ resources/views/Client/ClientAppointment.php
@@ -91,12 +91,12 @@
                             <div class="row">
                                 <div class="col-auto">
                                     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                                    <div class="mb-3">
-                                        <select name="category" class="form-control" id="caseType" required="" onchange="updateLawyerOptions(this.value)">
-                                            <option value="" disabled selected>Select a category</option>
-                                            <?php echo $CatogeryOptions; ?>
-                                        </select>
-                                    </div>
+                                        <div class="mb-3">
+                                            <select name="category" class="form-control" id="caseType" required="" onchange="updateLawyerOptions(this.value)">
+                                                <option value="" disabled selected>Select a category</option>
+                                                <?php echo $CatogeryOptions; ?>
+                                            </select>
+                                        </div>
                                 </div>
                                 <div class="col-auto">
                                     <div class="mb-3">
@@ -105,23 +105,23 @@
                                         </select>
                                     </div>
                                 </div>
-                                    <div class="col-auto">
-                                        <div class="mb-3">
-                                            <select name="time" class="form-control" id="dateAndTimeDropdown" required="">
-                                                    onchange="updateLawyerOptions(this.value)">
-                                                <option value="" disabled selected></i>Select Date & Time</option>
-                                            </select>
-                                        </div>
+                                <div class="col-auto">
+                                    <div class="mb-3">
+                                        <select name="time" class="form-control" id="dateAndTimeDropdown" required="">
+                                            onchange="updateLawyerOptions(this.value)">
+                                            <option value="" disabled selected></i>Select Date & Time</option>
+                                        </select>
+                                    </div>
                                 </div>
                                 <div class="basic-form">
                                     <textarea name="description" id="textarea" class="form-control input-default fixed-textarea"
                                               placeholder="Type your notice here."></textarea>
-                                        <br>
-                                        <button type="submit" name="submit" class="btn btn-primary btn-sm">Send
-                                            Statement
-                                        </button>
+                                    <br>
+                                    <button type="submit" name="submit" class="btn btn-primary btn-sm">Send
+                                        Statement
+                                    </button>
                                 </div>
-                              
+
                                 <style>
                                     .fixed-textarea {
                                         resize: none;
@@ -191,5 +191,4 @@
         }
     }
 </script>
-//
-//
+//re
