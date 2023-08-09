e61f7d1a174d4001362152edceb9654eb421cc65
Thimira Sadeesha <sadeeshathimira2000@gmail.com>
2023-08-10 02:00:32 +0530

    Remove Profile From SB

--- resources/views/Lawyer/Sidebar.php
+++ resources/views/Lawyer/Sidebar.php
@@ -113,15 +113,7 @@
                         <img src="../../images/user.png" width="56" alt="">
                     </a>
                     <div class="dropdown-menu dropdown-menu-end">
-                        <a href="app-profile.html" class="dropdown-item ai-icon">
-                            <svg id="icon-user1" xmlns="http://www.w3.org/2000/svg" class="text-primary" width="18"
-                                 height="18" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                 stroke-linecap="round" stroke-linejoin="round">
-                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                                <circle cx="12" cy="7" r="4"></circle>
-                            </svg>
-                            <span class="ms-2">Profile </span>
-                        </a>
+
                         <a href="logout.php" class="dropdown-item ai-icon">
                             <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18"
                                  viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
