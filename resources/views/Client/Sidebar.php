05556dfd9168c573269b8f0ca569528e9835ee25
sooriyo <tharukaravisara@gmail.com>
2023-08-11 09:03:43 +0530

    Sidebar Updated

--- resources/views/Client/Sidebar.php
+++ resources/views/Client/Sidebar.php
@@ -6,7 +6,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content="https:/fillow.dexignlab.com/xhtml/social-image.png">
     <meta name="format-detection" content="telephone=no">
     <link href="../../vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
     <link href="../../vendor/owl-carousel/owl.carousel.css" rel="stylesheet">
@@ -31,17 +30,6 @@
 
 <div class="nav-header">
     <a class="brand-logo">
-        <svg class="logo-abbr" width="55" height="55" viewbox="0 0 55 55" fill="none"
-             xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M27.5 0C12.3122 0 0 12.3122 0 27.5C0 42.6878 12.3122 55 27.5 55C42.6878 55
-                   55 42.6878 55 27.5C55 12.3122 42.6878 0 27.5 0ZM28.0092 46H19L19.0001 34.9784L19 27.5803V24.4779C19 
-                   14.3752 24.0922 10 35.3733 10V17.5571C29.8894 17.5571 28.0092 19.4663 28.0092 24.4779V27.
-                   5803H36V34.9784H28.0092V46Z"
-                  fill="url(#paint0_linear)"></path>
-            <defs>
-            </defs>
-        </svg>
 
         <?php
 
